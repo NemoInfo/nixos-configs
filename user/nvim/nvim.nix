a75962c66ec99f718a631a105125f6c9a4399cf7
Aaron <aaronpanaitescu@gmail.com>
2025-04-17 14:18:18 +0100

    Add typst, stabalize config

--- user/nvim/nvim.nix
+++ user/nvim/nvim.nix
@@ -1,37 +1,40 @@
-{ pkgs, ... }: {
+{ pkgs, latest, ... }: {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
-    plugins = with pkgs.vimPlugins; [
-      packer-nvim
-      lualine-nvim
-      rose-pine
-      telescope-fzf-native-nvim
-      kanagawa-nvim
-      nvim-cmp
-      cmp-nvim-lsp
-      cmp-nvim-lua
-      nvim-lspconfig
-      cmp-buffer
-      cmp-path
-      cmp-cmdline
-      null-ls-nvim
-      nvim-web-devicons
-      oil-nvim
-      alpha-nvim
-      vimtex
-      lazygit-nvim
-      vim-fugitive
-      rust-tools-nvim
-      coc-ltex
-      julia-vim
-      # typst-preview-nvim # where are these?? 
-      # typst-conceal-vim
-      # nvim-treesitter-parsers.typst
-      typst-vim
-    ];
+    plugins = let
+      old = with pkgs.vimPlugins; [
+        packer-nvim
+        lualine-nvim
+        rose-pine
+        telescope-fzf-native-nvim
+        kanagawa-nvim
+        nvim-cmp
+        cmp-nvim-lsp
+        cmp-nvim-lua
+        nvim-lspconfig
+        cmp-buffer
+        cmp-path
+        cmp-cmdline
+        null-ls-nvim
+        nvim-web-devicons
+        oil-nvim
+        alpha-nvim
+        vimtex
+        lazygit-nvim
+        vim-fugitive
+        rust-tools-nvim
+        coc-ltex
+        julia-vim
+        typst-vim
+      ];
+      new = with latest.legacyPackages.x86_64-linux.vimPlugins; [
+        typst-preview-nvim
+        typst-vim
+      ];
+    in old ++ new;
   };
 
   home.file.".config/nvim/init.lua".source = ./init.lua;
--- user/packages.nix
+++ user/packages.nix
@@ -1,53 +1,57 @@
 { pkgs, latest, ... }: {
-  home.packages = with pkgs; [
-    # apps
-    sioyek
-    google-chrome
-    alacritty
-    icon-library
-    nerdfonts
-    discord
-    inkscape
-    obs-studio
-    vlc
-    # bottles
-    # virtualbox
+  home.packages = let
+    pkgList = with pkgs; [
+      # apps
+      sioyek
+      google-chrome
+      alacritty
+      icon-library
+      nerdfonts
+      discord
+      inkscape
+      obs-studio
+      vlc
+      # bottles
+      # virtualbox
 
-    # cli tools
-    ripgrep
-    lazygit
-    neofetch
-    cmatrix
-    railway
-    cloc
-    tree
+      # cli tools
+      ripgrep
+      lazygit
+      neofetch
+      cmatrix
+      railway
+      cloc
+      tree
 
-    # languages
-    gcc # C compiler
-    clang-tools # CPP lsp
-    nil # Nix lsp
-    haskellPackages.nixfmt # Nix fmt
-    lua-language-server # Lua lsp
-    stylua # Lua fmt
-    texlab # TeX Lsp
-    (pkgs.texlive.combine {
-      inherit (pkgs.texlive)
-        scheme-medium # The basics
-        cleveref # Smart referencing
-        enumitem adjustbox upquote tcolorbox pdfcol environ # Jupyter export
-        glossaries glossaries-english glossaries-extra pdfcomment datetime2
-        datetime2-english zref marginnote soulpos svg transparent xcolor # .
-        catchfile appendix titling;
-    }) # TeX
-    biber # BibLaTex backend
-    pyright # Python Lsp
-    yapf # Pythin fmt
-    rust-analyzer # Rust lsp
-    rustfmt # Rust fmt
-    rustc # Rust compiler
-    cargo # Rust dependecy manager
-    cudatoolkit
-    (latest.legacyPackages.x86_64-linux.typst) # f*ck latex
-    tinymist # typst lsp
-  ];
+      # languages
+      gcc # C compiler
+      clang-tools # CPP lsp
+      nil # Nix lsp
+      haskellPackages.nixfmt # Nix fmt
+      lua-language-server # Lua lsp
+      stylua # Lua fmt
+      texlab # TeX Lsp
+      (pkgs.texlive.combine {
+        inherit (pkgs.texlive)
+          scheme-medium # The basics
+          cleveref # Smart referencing
+          enumitem adjustbox upquote tcolorbox pdfcol environ # Jupyter export
+          glossaries glossaries-english glossaries-extra pdfcomment datetime2
+          datetime2-english zref marginnote soulpos svg transparent xcolor # .
+          catchfile appendix titling;
+      }) # TeX
+      biber # BibLaTex backend
+      pyright # Python Lsp
+      yapf # Pythin fmt
+      rust-analyzer # Rust lsp
+      rustfmt # Rust fmt
+      rustc # Rust compiler
+      cargo # Rust dependecy manager
+      cudatoolkit
+    ];
+    latestList = with latest.legacyPackages.x86_64-linux; [
+      typst # f*ck latex
+      tinymist
+    ];
+  in pkgList ++ latestList;
 }
